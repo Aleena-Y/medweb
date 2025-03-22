25fe6c30403e21e88cb1c862992a0d762de773fd
Aleena Yogindar <aleenayogi@gmail.com>
2025-03-22 20:08:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Troubleshooter AI',
+    version= '0.0.0',
+    author= 'Aleena Yogindar',
+    author_email= 'aleenayogi@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
